9eba57c68867075a91e1f3603e551d981e7b7ad9
School Of Code Staff <staff@kdp4pcflwr.home>
2022-09-20 14:15:58 +0100

    changed server file to be on vercel

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
 export const server = dev
   ? "http://localhost:3000"
-  : "https://italianbowls.netlify.app";
+  : "https://italian-resturant-front-end.vercel.app/";
 // if in production use there server else use local host
